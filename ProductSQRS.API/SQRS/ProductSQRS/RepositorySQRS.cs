034e84317cea1f240182a0d08b4fc95ec54654d7
chivy140820a <chivy1408@gmail.com>
2021-10-08 11:50:13 +0700

    UpdateGeneric

--- ProductSQRS.API/SQRS/ProductSQRS/RepositorySQRS.cs
+++ ProductSQRS.API/SQRS/ProductSQRS/RepositorySQRS.cs
@@ -1,6 +1,28 @@
-﻿namespace ProductSQRS.API.SQRS.ProductSQRS
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProductSQRS.API.Data;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductSQRS.API.SQRS.ProductSQRS
 {
-    public class RepositorySQRS
+    public class RepositorySQRS<T> where T:class
     {
+        public record Query():IRequest<List<T>>;
+        public class Handler : IRequestHandler<Query, List<T>>
+        {
+            private readonly ApplicationDbContext _context;
+            public DbSet<T> table = null;
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+                table = _context.Set<T>();
+            }    
+            public async Task<List<T>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await table.ToListAsync();
+            }
+        }
     }
 }
